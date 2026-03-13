bc498ba72ac708dd95162e19da1dd17660cd2089
christian-fx <akabuezechris432@gmail.com>
2026-03-13 19:38:18 +0100

    feat: add github username validation util

---
+++ src/utils/github.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+async function validateUsername(username) {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        return response.data;
+    } catch (error) {
+        if (error.response && error.response.status === 404) {
+            throw new Error(`GitHub user "${username}" not found.`);
+        }
+        throw new Error(`Could not reach GitHub API. Check your internet connection.`);
+    }
+}
+
+module.exports = { validateUsername };
+
+validateUsername('allwellazubike').then(data => console.log(data.name, data.bio));
